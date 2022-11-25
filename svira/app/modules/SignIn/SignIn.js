b7caea89c1ecdfcf6a5012c974291299797ad09f
chirag ramani <ramanichirag069@gmail.com>
2022-11-25 10:08:34 +0530

    sign in status bar change

--- svira/app/modules/SignIn/SignIn.js
+++ svira/app/modules/SignIn/SignIn.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "react-native-paper";
+import React, {useState, useEffect} from 'react';
+import {Button} from 'react-native-paper';
 import {
   View,
   Image,
@@ -13,175 +13,165 @@ import {
   ScrollView,
   TouchableOpacity,
   SafeAreaView,
-} from "react-native";
-import { Formik } from "formik";
-import * as yup from "yup";
-import NetInfo from "@react-native-community/netinfo";
-import Toast from "react-native-toast-message";
+} from 'react-native';
+import {Formik} from 'formik';
+import * as yup from 'yup';
+import NetInfo from '@react-native-community/netinfo';
+import Toast from 'react-native-toast-message';
 
-import { TextInput as RNINPUT } from "react-native-paper";
-import { useSelector, useDispatch } from "react-redux";
-import { setBgColor } from "../../redux/actions/themeActions.js";
-import Spinner from "react-native-loading-spinner-overlay";
-import { signInAction } from "../../redux/actions/authActons.js";
-import { sc, vsc, msc } from "../../appConstants/Utils";
-import {  SafeAreaProvider, useSafeAreaInsets} from 'react-native-safe-area-context';
-
-
-
-
-
+import {TextInput as RNINPUT} from 'react-native-paper';
+import {useSelector, useDispatch} from 'react-redux';
+import {setBgColor} from '../../redux/actions/themeActions.js';
+import Spinner from 'react-native-loading-spinner-overlay';
+import {signInAction} from '../../redux/actions/authActons.js';
+import {sc, vsc, msc} from '../../appConstants/Utils';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 const singInCheck = yup.object({
-  name: yup.string().required("mobile number is required"),
-  password: yup.string().required("password is required"),
+  name: yup.string().required('mobile number is required'),
+  password: yup.string().required('password is required'),
 });
 
-const SignIn = ({ navigation }) => {
+const SignIn = ({navigation}) => {
   const dispatch = useDispatch();
 
-  const { userToken, LoginInLoading } = useSelector((state) => state.authState);
+  const {userToken, LoginInLoading} = useSelector(state => state.authState);
 
   useEffect(() => {
     if (userToken) {
-      navigation.navigate("Home");
+      navigation.navigate('Home');
     }
   }, [navigation, userToken]);
 
   const Touchable =
-    Platform.OS === "ios" ? TouchableHighlight : TouchableNativeFeedback;
+    Platform.OS === 'ios' ? TouchableHighlight : TouchableNativeFeedback;
 
   if (userToken) {
-    dispatch(setBgColor("#db9b7b"));
+    dispatch(setBgColor('#db9b7b'));
   }
 
-  const CustomStatusBar = (
-    {
-      backgroundColor,
-      barStyle = "dark-content",
-      //add more props StatusBar
-    }
-  ) => { 
-     
-     const insets = useSafeAreaInsets();
-  
-     return (
-  
-       <View style={{ height: insets.top, backgroundColor }}>
-          <StatusBar
-            animated={true}
-            backgroundColor={backgroundColor}
-            barStyle={barStyle} />
-       </View>
-     );
-  }
+  const CustomStatusBar = ({
+    backgroundColor,
+    barStyle = 'dark-content',
+    //add more props StatusBar
+  }) => {
+    const insets = useSafeAreaInsets();
+
+    return (
+      <View style={{height: insets.top, backgroundColor}}>
+        <StatusBar
+          animated={true}
+          backgroundColor={backgroundColor}
+          barStyle={barStyle}
+        />
+      </View>
+    );
+  };
 
   return (
     <>
-
-
       <Spinner
         visible={LoginInLoading}
-        textContent={"Loading..."}
-        textStyle={{ color: "#fff", fontSize: vsc(14)}}
+        textContent={'Loading...'}
+        textStyle={{color: '#fff', fontSize: vsc(14)}}
         overlayColor="rgba(0,0,0, 0.5)"
       />
 
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFEFD" }}>
+      <SafeAreaView style={{flex: 1, backgroundColor: '#FFFEFD'}}>
+        <CustomStatusBar backgroundColor="#FFFEFD" />
         <ScrollView
           keyboardShouldPersistTaps="handled"
           showsVerticalScrollIndicator={false}
           showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: vsc(20) }}
-        >
+          contentContainerStyle={{paddingBottom: vsc(20)}}>
           <View
             style={{
-              justifyContent: "center",
-              alignItems: "center",
+              justifyContent: 'center',
+              alignItems: 'center',
               marginTop: vsc(20),
-            }}
-          >
+            }}>
             <Image
-              style={{ width: sc(120), height: sc(120), resizeMode: "contain" }}
-              source={require("../../assets/header1.png")}
+              style={{width: sc(120), height: sc(120), resizeMode: 'contain'}}
+              source={require('../../assets/header1.png')}
             />
           </View>
           <View
             style={{
               marginTop: vsc(20),
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
             <Text
               style={{
                 fontSize: vsc(25),
-                fontFamily: "Cairo-Regular",
-                color: "#000000",
-              }}
-            >
+                fontFamily: 'Cairo-Regular',
+                color: '#000000',
+              }}>
               Customer Sign In
             </Text>
           </View>
 
           <Formik
             initialValues={{
-              name: "",
-              password: "",
+              name: '',
+              password: '',
             }}
             validationSchema={singInCheck}
             onSubmit={(values, actions) => {
-              NetInfo.fetch().then((state) => {
+              NetInfo.fetch().then(state => {
                 if (state.isConnected) {
                   actions.resetForm();
                   dispatch(signInAction(values.name, values.password));
                 } else {
                   Toast.show({
-                    text1: "Check your Internet Connection",
+                    text1: 'Check your Internet Connection',
                     visibilityTime: 3000,
                     autoHide: true,
-                    position: "bottom",
-                    type: "error",
+                    position: 'bottom',
+                    type: 'error',
                   });
                 }
               });
-            }}
-          >
-            {(props) => (
-              <View style={{ marginHorizontal: sc(30) }}>
+            }}>
+            {props => (
+              <View style={{marginHorizontal: sc(30)}}>
                 <View
                   style={{
                     marginBottom: vsc(20),
                     marginTop: vsc(25),
-                  }}
-                >
+                  }}>
                   <TextInput
                     underlineColorAndroid="transparent"
-                    selectionColor={"#db9b7b"}
+                    selectionColor={'#db9b7b'}
                     placeholder="Mobile No."
                     placeholderTextColor="#666666"
                     keyboardType="numeric"
                     style={{
-                      color: "#000000",
+                      color: '#000000',
                       fontSize: vsc(20),
 
                       borderWidth: 0,
                     }}
-                    onChangeText={props.handleChange("name")}
-                    onBlur={props.handleBlur("name")}
+                    onChangeText={props.handleChange('name')}
+                    onBlur={props.handleBlur('name')}
                     value={props.values.name}
                   />
                   <View
                     style={{
                       marginTop: vsc(7),
                       elevation: 4,
-                      backgroundColor: "#db9b7b",
+                      backgroundColor: '#db9b7b',
                       paddingVertical:
-                        Dimensions.get("window").height < 600 ? vsc(0.7) : vsc(0.6),
+                        Dimensions.get('window').height < 600
+                          ? vsc(0.7)
+                          : vsc(0.6),
                     }}
                   />
-                  <View style={{ marginBottom: vsc(13), marginTop: vsc(3) }}>
-                    <Text style={{ color: "red", fontSize: vsc(14)}}>
+                  <View style={{marginBottom: vsc(13), marginTop: vsc(3)}}>
+                    <Text style={{color: 'red', fontSize: vsc(14)}}>
                       {props.touched.name && props.errors.name}
                     </Text>
                   </View>
@@ -192,34 +182,35 @@ const SignIn = ({ navigation }) => {
                     marginBottom: vsc(20),
                     marginTop: vsc(0),
                     // marginHorizontal: 20,
-                  }}
-                >
+                  }}>
                   <TextInput
                     underlineColorAndroid="transparent"
                     placeholder="Password"
                     placeholderTextColor="#666666"
                     secureTextEntry={true}
-                    selectionColor={"#db9b7b"}
+                    selectionColor={'#db9b7b'}
                     style={{
                       fontSize: vsc(20),
-                      color: "#000000",
+                      color: '#000000',
                       borderWidth: 0,
                     }}
-                    onChangeText={props.handleChange("password")}
-                    onBlur={props.handleBlur("password")}
+                    onChangeText={props.handleChange('password')}
+                    onBlur={props.handleBlur('password')}
                     value={props.values.password}
                   />
                   <View
                     style={{
                       marginTop: vsc(7),
                       elevation: 4,
-                      backgroundColor: "#db9b7b",
+                      backgroundColor: '#db9b7b',
                       paddingVertical:
-                        Dimensions.get("window").height < 600 ? vsc(0.7) : vsc(0.7),
+                        Dimensions.get('window').height < 600
+                          ? vsc(0.7)
+                          : vsc(0.7),
                     }}
                   />
-                  <View style={{ marginBottom: vsc(13), marginTop: vsc(3) }}>
-                    <Text style={{ color: "red", fontSize: vsc(14)}}>
+                  <View style={{marginBottom: vsc(13), marginTop: vsc(3)}}>
+                    <Text style={{color: 'red', fontSize: vsc(14)}}>
                       {props.touched.password && props.errors.password}
                     </Text>
                   </View>
@@ -227,39 +218,34 @@ const SignIn = ({ navigation }) => {
 
                 <View
                   style={{
-                    alignItems: "center",
-                    justifyContent: "center",
+                    alignItems: 'center',
+                    justifyContent: 'center',
                     // marginBottom: 15,
-                  }}
-                >
+                  }}>
                   <View
                     style={{
-                      overflow: "hidden",
+                      overflow: 'hidden',
                       borderRadius: sc(50),
-                    }}
-                  >
+                    }}>
                     <Touchable
-                      background={TouchableNativeFeedback.Ripple("#ECECEC")}
+                      background={TouchableNativeFeedback.Ripple('#ECECEC')}
                       onPress={() => {
                         props.handleSubmit();
-                      }}
-                    >
+                      }}>
                       <View
                         style={{
                           borderRadius: vsc(50),
-                          width: Dimensions.get("window").width / 1.5,
+                          width: Dimensions.get('window').width / 1.5,
                           paddingVertical: vsc(15),
-                          backgroundColor: "#db9b7b",
-                          fontFamily: "Play-Regular",
-                        }}
-                      >
+                          backgroundColor: '#db9b7b',
+                          fontFamily: 'Play-Regular',
+                        }}>
                         <Text
                           style={{
-                            textAlign: "center",
-                            color: "#FFFFFF",
+                            textAlign: 'center',
+                            color: '#FFFFFF',
                             fontSize: vsc(17),
-                          }}
-                        >
+                          }}>
                           SIGN IN
                         </Text>
                       </View>
@@ -269,42 +255,37 @@ const SignIn = ({ navigation }) => {
 
                 <View
                   style={{
-                    alignItems: "center",
-                    justifyContent: "center",
+                    alignItems: 'center',
+                    justifyContent: 'center',
                     marginTop: vsc(12),
-                  }}
-                >
+                  }}>
                   <View
                     style={{
-                      overflow: "hidden",
+                      overflow: 'hidden',
                       borderRadius: vsc(50),
-                    }}
-                  >
+                    }}>
                     <Touchable
-                      background={TouchableNativeFeedback.Ripple("#ECECEC")}
+                      background={TouchableNativeFeedback.Ripple('#ECECEC')}
                       onPress={() => {
                         navigation.reset({
                           index: 0,
-                          routes: [{ name: "Home" }],
+                          routes: [{name: 'Home'}],
                         });
-                      }}
-                    >
+                      }}>
                       <View
                         style={{
                           borderRadius: vsc(50),
-                          width: Dimensions.get("window").width / 1.5,
+                          width: Dimensions.get('window').width / 1.5,
                           paddingVertical: vsc(15),
-                          backgroundColor: "#db9b7b",
-                          fontFamily: "Play-Regular",
-                        }}
-                      >
+                          backgroundColor: '#db9b7b',
+                          fontFamily: 'Play-Regular',
+                        }}>
                         <Text
                           style={{
-                            textAlign: "center",
-                            color: "#FFFFFF",
+                            textAlign: 'center',
+                            color: '#FFFFFF',
                             fontSize: vsc(17),
-                          }}
-                        >
+                          }}>
                           GUEST USER
                         </Text>
                       </View>
@@ -321,28 +302,27 @@ const SignIn = ({ navigation }) => {
               marginHorizontal: sc(30),
               paddingVertical: vsc(0.4),
               elevation: 1,
-              backgroundColor: "#DDDDDD",
+              backgroundColor: '#DDDDDD',
             }}
           />
           <View
             style={{
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
               marginTop: vsc(10),
               marginBottom: vsc(10),
-            }}
-          >
-            <Text style={{ color: "#666666", fontSize: vsc(14) }}>
-              You are not a registered use click{" "}
+            }}>
+            <Text style={{color: '#666666', fontSize: vsc(14)}}>
+              You are not a registered use click{' '}
             </Text>
-            <TouchableOpacity onPress={() => navigation.navigate("ContactUs")}>
-              <Text style={{ color: "#666666" , fontSize: vsc(14)}}>here</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('ContactUs')}>
+              <Text style={{color: '#666666', fontSize: vsc(14)}}>here</Text>
               <View
                 style={{
                   paddingVertical:
-                    Dimensions.get("window").height < 600 ? vsc(0.7) : vsc(0.6),
-                  backgroundColor: "#000000",
+                    Dimensions.get('window').height < 600 ? vsc(0.7) : vsc(0.6),
+                  backgroundColor: '#000000',
                 }}
               />
             </TouchableOpacity>
